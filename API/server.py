8bf3399ddaa0c9b59414c572fb400775e8a5e9b3
HuyNV <nguyenvohuy28@gmail.com>
2018-07-25 20:00:30 +0700

    modify sth

--- API/server.py
+++ API/server.py
@@ -2,38 +2,51 @@
 
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from sklearn.externals import joblib
 from json import dumps
 
 app = Flask(__name__)
 api = Api(app)
+# load model from file
+filename = "finalized_model.sav"
+model = joblib.load(filename)
 
-features=["SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "DROP", "ALTER", "RENAME", "WHERE", "FROM", "UNION", "NOT", "AND", "OR", "XOR","EXEC","!", "&&", "||", "--", "#", "<", ">", "<=>", ">=", "<=", "==", "=", "!=", "<<", ">>", "<>", "%", "*", "?", "|", "&", "-", "+","/**/"]
+features = ["SELECT", "INSERT", "UPDATE", "DELETE", "CREATE", "DROP", "ALTER", "RENAME", "WHERE", "FROM", "UNION",
+            "NOT", "AND", "OR", "XOR", "EXEC", "!", "&&", "||", "--", "#", "<", ">", "<=>", ">=", "<=", "==", "=", "!=",
+            "<<", ">>", "<>", "%", "*", "?", "|", "&", "-", "+", "/**/"]
+
 
 # process raw data into features to classify
 def processRawData(raw_url):
-	array=[]
-	array.append(len(raw_url))
-	for x in features:
-	    print(x +": ")
-    	if raw_url.lower().__contains__(x.lower()):
-    	   print('yes')     #    	array.append(1)
-   	 	else:
-		   print('no')
+    array = []
+    array.append(len(raw_url))
+    for x in features:
+        if raw_url.lower().__contains__(x.lower()):
+            # print('yes')
+            array.append(1)
+        else:
+            # print('no')
+            array.append(0)
+    print(array.__len__())
+    return array
 
-	return array
 
 # using ML Model to classify
 def classify(record):
-	return "test"
+    return model.predict([record])[0]
 
 
 class Raws(Resource):
-	def get(self,raw_url):
-		data = processRawData(raw_url)
-		result={'code':200,'isMalicious':'false','data':data}
-		return jsonify(result)
+    def post(self):
+        raw_url = request.json['raw']
+        data = processRawData(raw_url)
+        checkresult = classify(data)
+        print (checkresult)
+        result = {'code': 200, 'isMalicious': checkresult, 'data': checkresult}
+        return jsonify(result)
 
-api.add_resource(Raws,'/classify/<raw_url>')
+
+api.add_resource(Raws, '/classify')
 
 if __name__ == '__main__':
-	app.run(port='5002')
+    app.run(port='5002')
